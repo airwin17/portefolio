765f39c77bc6470863520b0a92971207142eb05c
airwin17 <faitourdja@gmail.com>
2023-07-08 01:52:38 +0100

    add slides

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
+
 var dark=true;
 
-function turnDark(){
+
+function changeTheme(){
     var root=document.querySelector(":root")
     if(dark==true){
         root.style.setProperty("--background","white");
+        root.style.setProperty("--background-second","#f0f1f3")
         root.style.setProperty("--background-solid","#1E3050");
         root.style.setProperty("--background-text","black");
+        themeBut.children[0].style.display="none";
+        themeBut.children[1].style.display="block";
         dark=false;
     }else{
-        root.style.setProperty("--background","#1E3050");
+        root.style.setProperty("--background","rgb(31, 49, 81)");
+        root.style.setProperty("--background-second","rgb(37 58 96)")
         root.style.setProperty("--background-solid","white");
         root.style.setProperty("--background-text","white");
+        themeBut.children[1].style.display="none";
+        themeBut.children[0].style.display="block";
         dark=true;
     }
 }
@@ -25,4 +33,30 @@ function drop(ind){
         icon.style.rotate="0deg";
     }
 }
+
 /*--------------------------------*/
+var themeBut=document.querySelector("#theme-but");
+themeBut.addEventListener("click",()=>changeTheme());
+
+/*---------------------------------*/
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
